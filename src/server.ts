73a73dc098d162491d0bc947b3c59ab0ff94ef92
Diego Antonio <suporte.diegociara@gmail.com>
2023-05-20 20:55:17 -0300

    https

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,20 @@
-import app from './app';
+// import app from './app';
 
-app.listen(process.env.PORT || 3001, () => console.log('Server on'));
+// app.listen(process.env.PORT || 3001, () => console.log('Server on'));
+
+
+
+import app from './app';
+const https = require("https");
+import {readFileSync} from "fs"
+import {join} from "path"
+var privateKey = readFileSync(join(__dirname, "key.pem"));
+var certificate = readFileSync(join(__dirname, "cert.pem"));
+https
+  .createServer({
+    key: privateKey,
+    cert: certificate,
+  },app)
+  .listen(3001, ()=>{
+    console.log('server is runing at port 3001')
+  });
